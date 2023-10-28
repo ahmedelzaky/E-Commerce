fd7a2ca8e043a2d27cc5242ec0004d36fb647833
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-10-28 21:07:38 +0200

    update product card

--- frontend/src/component/GetProducts.js
+++ frontend/src/component/GetProducts.js
@@ -1,14 +1,18 @@
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 import useFetch from "./UseFetch";
 import { Col, Row } from "react-bootstrap";
 import "./css/products.css";
+import LoadingScreen from "./LoadingScreen";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const GetProducts = ({ url }) => {
-  const { data, isPending } = useFetch(url);
+  const { data, isPending, error } = useFetch(url);
   return (
     <Row>
-      {isPending && <p> lodaing... </p>}
+      {isPending && <LoadingScreen />}
+      {error && <p>{error}</p>}
       {data &&
         data.map((product) => (
           <Col key={product.id}>
@@ -23,18 +27,22 @@ const GetProducts = ({ url }) => {
                   }}
                 />
               </center>
-
               <Card.Body>
-                <Card.Title style={{ height: "60px", overflow: "hidden" }}>
-                  {product.title}
+                <Card.Title>
+                  {product.title.length > 35
+                    ? product.title.substring(0, 35) + "..."
+                    : product.title}
                 </Card.Title>
                 <Card.Text>
                   {product.description.length > 80
-                    ? product.description.substring(0, 80)
+                    ? product.description.substring(0, 80) + "..."
                     : product.description}
                 </Card.Text>
+                <p className="price">{product.price}$</p>
                 <center>
-                  <Button variant="primary">{product.price}$</Button>
+                  <Button>
+                    Add <FontAwesomeIcon icon={faCartShopping} />
+                  </Button>
                 </center>
               </Card.Body>
             </Card>
