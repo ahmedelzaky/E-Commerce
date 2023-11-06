e5f95855b967f7e0fdc015e6cecf363896ce4fd7
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-11-06 19:31:49 +0200

    limit 8 products per page

--- frontend/src/component/Products/GetProducts.js
+++ frontend/src/component/Products/GetProducts.js
@@ -8,57 +8,99 @@ import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../../rtk/slices/cart-slice";
 import { FaShoppingCart } from "react-icons/fa";
+import { BiSolidErrorAlt } from "react-icons/bi";
+import { useState } from "react";
 
 const GetProducts = ({ url }) => {
   const { data: products, isPending, error } = useFetch(url);
+
   const dispatch = useDispatch();
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const productsPerPage = 8;
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const pageNumbers = [];
+
+  let currentProducts = [];
+
+  if (products)
+    currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  if (products)
+    for (let i = 1; i <= Math.ceil(products.length / productsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <Row>
-      {isPending && <LoadingScreen />}
-      {error && <p>{error}</p>}
-      {products &&
-        products.map((product) => (
-          <Col key={product.id}>
-            <Card className="product-item">
-              <center>
-                <Card.Img
-                  variant="top"
-                  src={product.image}
-                  style={{
-                    height: "250px",
-                    width: "250px",
-                  }}
-                />
-              </center>
-              <Card.Body>
-                <Card.Title>
-                  <Link to={"/product/" + product.id}>
-                    {product.title.length > 35
-                      ? product.title.substring(0, 35) + "..."
-                      : product.title}
-                  </Link>
-                </Card.Title>
-                {/* <Card.Text>
-                  {product.description.length > 80
-                    ? product.description.substring(0, 80) + "..."
-                    : product.description}
-                </Card.Text> */}
-                <p className="price">{product.price}$</p>
+    <>
+      <Row id="#top">
+        {isPending && <LoadingScreen />}
+        {error && (
+          <div className="error">
+            <BiSolidErrorAlt className="error-icon" fontSize={"30px"} /> {error}
+          </div>
+        )}
+        {products &&
+          currentProducts.map((product) => (
+            <Col key={product.id}>
+              <Card className="product-item">
                 <center>
-                  <Button
-                    variant="warning"
-                    onClick={() => {
-                      dispatch(addToCart(product));
+                  <Card.Img
+                    variant="top"
+                    src={product.image}
+                    style={{
+                      height: "250px",
+                      width: "250px",
                     }}
-                  >
-                    Add <FaShoppingCart />
-                  </Button>
+                  />
                 </center>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-    </Row>
+                <Card.Body>
+                  <Card.Title>
+                    <Link to={"/product/" + product.id}>
+                      {product.title.length > 35
+                        ? product.title.substring(0, 35) + "..."
+                        : product.title}
+                    </Link>
+                  </Card.Title>
+                  <p className="price">{product.price}$</p>
+                  <center>
+                    <Button
+                      variant="warning"
+                      onClick={() => {
+                        dispatch(addToCart(product));
+                      }}
+                    >
+                      Add <FaShoppingCart />
+                    </Button>
+                  </center>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+      </Row>
+
+      {products && (
+        <ul className="pagination">
+          {pageNumbers.map((number) => (
+            <li key={number} className="page-item">
+              <a
+                href="#top"
+                onClick={() => handlePageClick(number)}
+                className="btn page-link"
+              >
+                {number}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 };
 
