4381e854bb71bd77cabba5a4064d34a2bc3f01a1
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2023-11-09 18:56:30 +0200

    create repos

---
+++ backend/src/main/java/com/example/backend/repositorys/AddressRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repositorys;
+
+import com.example.backend.models.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepository extends JpaRepository<Address, Long> {
+}
