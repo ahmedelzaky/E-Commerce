846d451b8ad67ec0c55df661dbfa9f7b21160b4a
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-11-24 20:27:41 +0200

    modify OpenApiConfig

--- backend/src/main/java/com/example/backend/config/OpenApiConfig.java
+++ backend/src/main/java/com/example/backend/config/OpenApiConfig.java
@@ -5,21 +5,33 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-@OpenAPIDefinition(info = @Info(
-        description = " OpenApi Documentation for E-Commerce",
-        title = "E-Commerce",
-        version = "0.5"
-), servers = {
-        @Server(
-                description = "Local Env",
-                url = "http://localhost:8080"
+@OpenAPIDefinition(
+        info = @Info(
+                description = " OpenApi Documentation for E-Commerce",
+                title = "E-Commerce",
+                version = "0.5"
         ),
-        @Server(
-                description = "Production Env",
-                url = "https://ecommerce-backend-9qsc.onrender.com"
-        ),
-})
+        servers = {
+                @Server(
+                        description = "Production Env",
+                        url = "https://ecommerce-backend-9qsc.onrender.com"
+                ),
+                @Server(
+                        description = "Local Env",
+                        url = "http://localhost:8080"
+                ),
+        }
+)
+@RestController
+@CrossOrigin
 public class OpenApiConfig {
-
+    @GetMapping(path = "/")
+    public RedirectView redirectToOpenApiDocs() {
+        return new RedirectView("/swagger-ui/index.html");
+    }
 }
