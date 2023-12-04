00881b2de72af321657e0f5f3dc62917ee607735
Ahmed Gad <116387176+ahmed-atef-gad@users.noreply.github.com>
2023-12-04 17:42:45 +0200

    create price history repo

--- backend/src/main/java/com/example/backend/models/PriceHistory.java
+++ backend/src/main/java/com/example/backend/models/PriceHistory.java
@@ -1,19 +1,24 @@
 package com.example.backend.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.Date;
 
+@Entity
+@Table(name = "Price_history_t")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class PriceHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(name = "product_id")
     private long product_id;
-    @Column(name = "price")
+    @Column(columnDefinition = "numeric")
     private Float price;
     @Column(name = "date", insertable = false)
     private Date date;
---
+++ backend/src/main/java/com/example/backend/repositorys/PriceHistoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repositorys;
+
+import com.example.backend.models.PriceHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PriceHistoryRepository extends JpaRepository<PriceHistory, Long> {
+}
