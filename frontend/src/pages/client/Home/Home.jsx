b2e658157e546ec01aa2d547d113beea88a13832
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-12-07 01:04:01 +0200

    Update category scaling and add top selling products section

--- frontend/src/pages/client/Home/Home.jsx
+++ frontend/src/pages/client/Home/Home.jsx
@@ -2,6 +2,8 @@ import "./home.css";
 import Categories from "../../../component/Categories/GetCategories";
 import Slider from "../../../component/Slider";
 import NavBar from "../../../component/NavBar/NavBar";
+import GetProducts from "../../../component/Product/GetProducts";
+import { Container } from "react-bootstrap";
 
 const Home = () => {
   return (
@@ -19,6 +21,10 @@ const Home = () => {
           />
         )}
         <Categories />
+        <Container className="top-selling">
+          <h2>Top Selling</h2>
+          <GetProducts url={`/products/top-selling`}></GetProducts>
+        </Container>
       </div>
     </NavBar>
   );
