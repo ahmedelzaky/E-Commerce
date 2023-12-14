f74f8ce9354d39c0990d0c465d7070bf54736b45
ahmedelzaky <ahmed.elzakistudy@gmail.com>
2023-12-15 01:39:07 +0200

    fix get address logic

--- backend/src/main/java/com/example/backend/services/AddressServices.java
+++ backend/src/main/java/com/example/backend/services/AddressServices.java
@@ -14,15 +14,11 @@ public class AddressServices {
     private AddressRepository addressRepository;
 
     public List<AddressDto> getAddressByCustomerId(Long customerId) {
-
-        List<AddressDto> addresses = addressRepository.CustomFindAddressByCustomerId(customerId);
-        if (addresses.isEmpty()) {
-            throw new IllegalStateException("this id " + customerId + " is not found");
-        }
-        return addresses;
+        return addressRepository.CustomFindAddressByCustomerId(customerId);
     }
 
     public void addAddress(Address address) {
+        System.out.println(address);
         addressRepository.save(address);
     }
 
