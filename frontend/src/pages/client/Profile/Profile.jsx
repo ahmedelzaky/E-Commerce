cfc78b8c888db7de50bc18fed916fe11e57ec698
RAHMA993 <romaa200399@gmail.com>
2023-12-17 17:04:21 +0200

    Profile page update

--- frontend/src/pages/client/Profile/Profile.jsx
+++ frontend/src/pages/client/Profile/Profile.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Button, Container, Row } from "react-bootstrap";
 import { USER } from "../../../api/auth";
 import NavBar from "../../../component/client/NavBar/NavBar";
@@ -7,6 +8,8 @@ import LoadingScreen from "../../../component/LoadingScreen";
 import ErrorMessage from "../../../component/ErrorMessage";
 import CartTable from "../../../component/CartTable";
 import { updateOrderStatus } from "../../../api/Server";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Profile = () => {
   const { data: customer, error, loading } = useAxios(`/customers/${USER.id}`);
@@ -22,56 +25,73 @@ const Profile = () => {
           {customer && (
             <Row className="details">
               <div className="customer">
-                <h4>Profile</h4>
+                <h4>
+                  <FontAwesomeIcon icon={faUser} /> Profile
+                </h4>
                 <h6>
-                  <span>First Name: {customer.firstName}</span>
-                  <span> Last Name: {customer.lastName}</span>
-                </h6>
-                <h6>
-                  <span> Email: {customer.email}</span>
-                  <span>Phone: {customer.phone}</span>
+                  <span className="label">First Name:</span>
+                  <span className="value">{customer.firstName}</span>
                 </h6>
 
-                <h6>Join Date: {new Date(customer.joinDate).toUTCString()} </h6>
+                <h6>
+                <span className="label">Last Name:</span>
+                <span className="value">{customer.lastName}</span>
+                </h6>
+
+                <h6>
+                  <span className="label"> Email:</span>
+                  <span className="value"> {customer.email}</span>
+                </h6>
+
+                <h6>
+                  <span className="label">Phone:</span>
+                  <span className="value"> {customer.phone}</span>
+                </h6>
+
+                <h6>
+                  <span  className="label">Join Date: </span>
+                  <span className="value"> {new Date(customer.joinDate).toUTCString()}</span>
+                </h6>
               </div>
               {orders?.length > 0 && (
                 <div className="orders">
                   {orders?.map((order) => (
                     <Row key={order.id}>
-                      <div className="oredr-headding">
-                        <h6>
-                          {" "}
-                          <span>Order #{order.id}</span>
-                          <span>
-                            Order Date {new Date(order.orderDate).toUTCString()}{" "}
-                          </span>
-                          {order.arrivalDate && (
-                            <span>
-                              {" "}
-                              Arriva Date{" "}
-                              {new Date(order.arrivalDate).toUTCString()}{" "}
-                            </span>
-                          )}
-                          <span
-                            className={`status ${
-                              order.statue == "COMPLETED"
-                                ? "Approved"
-                                : "Pending"
-                            }`}
-                          >
-                            {order.statue}{" "}
-                          </span>
-                          {order.statue == "PENDING" && (
-                            <Button
-                              onClick={() =>
-                                updateOrderStatus(order.id, "CANCELLED")
-                              }
-                              variant="danger"
-                            >
-                              Cancel
-                            </Button>
-                          )}
+                      <div className="order-headding">
+                      <h6>
+                        <span className="label">Order: </span>
+                        <span className="value">#{order.id}</span>
+                      </h6>
+                      <h6>
+                        <span className="label">Order Date: </span>
+                        <span className="value">{new Date(order.orderDate).toUTCString()}{" "}</span>
                         </h6>
+                        <h6>
+                    {order.arrivalDate && (
+                      <span>
+                        <span className="label"> Arrival Date: </span>
+                        <span className="value">{new Date(order.arrivalDate).toUTCString()}</span>
+                      </span>
+                    )}
+                        </h6>
+                        <h5>
+                          <span
+                          className={`status ${
+                            order.statue === "COMPLETED" ? "Approved" :
+                            order.statue === "PENDING" ? "Pending" : "Cancelled"
+                          }`}
+                        >
+                          {order.statue}{" "}
+                        </span>
+                        {order.statue === "PENDING" && (
+                          <Button
+                            onClick={() => updateOrderStatus(order.id, "CANCELLED")}
+                            variant="danger"
+                          >
+                            Cancel
+                          </Button>
+                        )}
+                        </h5>
                       </div>
                       <CartTable orderId={order.id} />
                       <hr />
